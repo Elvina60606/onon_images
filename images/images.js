9293e00e07fe7fe2cd091fc4d80231aca192325d
Elvina <cat250070@gmail.tw>
2026-03-05 10:34:31 +0800

    add images file

---
+++ images/images.js
@@ -0,0 +1,111 @@
+import avatar from './avatar.png';
+import basque from './basque.png';
+import bretonBiscuit from './bretonBiscuit.png';
+import cake from './cake.png';
+import cake2 from './cake2.png';
+import cake3 from './cake3.png';
+import canele from './canele.png';
+import canele1 from './canele1.png';
+import canele2 from './canele2.png';
+import caneleBanner from './caneleBanner.jpg';
+import caneleBannerMd from './caneleBannerMd.jpg';
+import caneleBg from './caneleBg.png';
+import caneleSub from './caneleSub.png';
+import caneleVector from './caneleVector.png';
+import caramelAlmondCanele from './caramelAlmondCanele.png';
+import chocolateBasque from './chocolateBasque.png';
+import contentcopy from './contentcopy.svg';
+import discountsBanner from './discountsBanner.jpg';
+import discountsBannerMd from './discountsBannerMd.jpg';
+import facebookWhite from './facebookWhite.svg';
+import hojichaCanele from './hojichaCanele.png';
+import instagramWhite from './instagramWhite.svg';
+import joinUs from './joinUs.png';
+import lemonCurdCanele from './lemonCurdCanele.png';
+import lemonMadeleine from './lemonMadeleine.png';
+import lineWhite from './lineWhite.svg';
+import loginHero from './loginHero.jpg';
+import logo3 from './logo3.svg';
+import madeleine from './madeleine.png';
+import madeleineMobile from './madeleineMobile.png';
+import midautumnfestivalBanner from './midautumnfestivalBanner.jpg';
+import midautumnfestivalBannerMd from './midautumnfestivalBannerMd.jpg';
+import ononLogo from './ononLogo.svg';
+import ononLogoSm from './ononLogoSm.svg';
+import ononLogoWhite from './ononLogoWhite.svg';
+import orangeChocolateCanele from './orangeChocolateCanele.png';
+import originalBasque from './originalBasque.png';
+import productCanele1 from './productCanele1.png';
+import productCanele2 from './productCanele2.png';
+import productCanele6 from './productCanele6.png';
+import rectangle303 from './rectangle303.png';
+import Rectangle303 from './Rectangle303.svg';
+import rectangle304 from './rectangle304.svg';
+import search from './search.svg';
+import subBg from './subBg.png';
+import vector1 from './vector1.png';
+import vector2 from './vector2.png';
+import vector3 from './vector3.png';
+import vector4 from './vector4.png';
+
+const images = {
+    //Home
+    basque,
+    cake,
+    cake2,
+    cake3,
+    canele,
+    canele2,
+    caneleSub,
+    caneleVector,
+    caneleBanner,
+    caneleBannerMd,
+    discountsBanner,
+    discountsBannerMd,
+    joinUs,
+    lemonMadeleine,
+    midautumnfestivalBanner,
+    midautumnfestivalBannerMd,
+    rectangle303,
+    rectangle304,
+    vector1,
+    vector2,
+    vector3,
+    vector4,
+
+    //Coupon
+    logo3,
+    contentcopy,
+    
+    avatar,
+    
+    bretonBiscuit,
+    canele1,
+    caneleBg,
+    caramelAlmondCanele,
+    chocolateBasque,
+    facebookWhite,
+    hojichaCanele,
+    instagramWhite,
+    lemonCurdCanele,
+    lineWhite,
+    loginHero,
+    madeleine,
+    madeleineMobile,
+    ononLogo,
+    ononLogoSm,
+    ononLogoWhite,
+    orangeChocolateCanele,
+    originalBasque,
+    productCanele1,
+    productCanele2,
+    productCanele6,
+    
+    Rectangle303,
+    
+    search,
+    subBg,
+    
+}
+
+export default images;
